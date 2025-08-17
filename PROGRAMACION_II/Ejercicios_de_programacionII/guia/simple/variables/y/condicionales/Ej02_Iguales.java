ccdd31edaf66d6dcba0d83b192307aa25d856286
EmiDBcode <Emilianoleiva20j@gmail.com>
2025-08-16 21:43:58 -0300

    Se realiza ejercico 2 de la guia.simple

---
+++ PROGRAMACION_II/Ejercicios_de_programacionII/guia/simple/variables/y/condicionales/Ej02_Iguales.java
@@ -0,0 +1,16 @@
+package guia.simple.variables.y.condicionales;
+
+public class Ej02_Iguales {
+
+    //compara
+
+    public static void comparaNumeros (int n1,int n2){
+
+        if (n1==n2){
+            System.out.println("El numero es igual");
+        }
+        else{
+            System.out.println("El numero no es igual");
+        }
+    }
+}
--- PROGRAMACION_II/Ejercicios_de_programacionII/guia/simple/variables/y/condicionales/MainGuiaSimpleTP.java
+++ PROGRAMACION_II/Ejercicios_de_programacionII/guia/simple/variables/y/condicionales/MainGuiaSimpleTP.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 class MainGuiaSimpleTP extends javax.swing.JFrame {
     public static void main(String[] args) {
 
+        //------------------------------------------------------
+        //-----------EJERCICIO_01-------------------------------
+
         // Mensaje que se vera pór pantalla
         System.out.println("Calcularemos el AREA de un circulo, para eso necesitamos el radio...");
 
@@ -18,6 +21,24 @@ class MainGuiaSimpleTP extends javax.swing.JFrame {
 
         // Impresion
         System.out.printf("El area de nuestro circulo es de: " + resultado);
+
+        //-------------------------------------------------------
+        //-------------EJERCICIO_02------------------------------
+        //ingresamos numeros por teclado
+        System.out.print("\n"+"Ingrese el primer número: ");
+        int primer_numero = sc.nextInt();
+
+        System.out.print("\n"+"Ingrese el segundo número: ");
+        int segundo_numero = sc.nextInt();
+
+        //Llamada de nuestra funcion, con entrada de valores
+        Ej02_Iguales.comparaNumeros(primer_numero,segundo_numero);
+
+
+
+
+
+
 
     }
 }
